10ef186f50e3bdd3a21084d3bfb0c5a8ce3d4b32
Janith Leanage <janithleanage@gmail.com>
2018-07-08 08:20:09 +0530

    added basic redux setup with redux persist

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./styles.css";
 import Navbar from "./common/Navbar";
 import AppContainer from "./common/AppContainer";
+import { store, persistor } from "./store";
 
 ReactDOM.render(
-  <React.Fragment>
-    <Navbar title="Three Point Estimation App" />
-    <AppContainer />
-  </React.Fragment>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <React.Fragment>
+        <Navbar title="Three Point Estimation App" />
+        <AppContainer />
+      </React.Fragment>
+    </PersistGate>
+  </Provider>,
   document.getElementById("root")
 );
